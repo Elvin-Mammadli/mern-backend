935163c4327ca97bd75c52eecbe0a4783d50b604
Elvin Mammadli <mamedli.elvin@gmail.com>
2022-03-09 13:47:05 +0400

    simple mern-stack backend

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const UserModel = require('./Users');
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb+srv://mernfirst:12345@backend-practice.z2wab.mongodb.net/mern-first?retryWrites=true&w=majority")
+
+app.get('/getUsers', (req, res) => {
+  UserModel.find({}, (err, data) => {
+    if(err) {
+      res.json(err);
+    } else {
+      res.json(data);
+    }
+  })
+})
+
+app.post('/createUser', async (req, res) => {
+  const user = req.body;
+  const newUser = new UserModel(user);
+  await newUser.save();
+
+  res.json(user);
+});
+
+app.put('/updateUser', (req, res) => {
+  console.log(req.body)
+  const id = req.body.id
+  const newName = req.body.name;
+  const newAge =   req.body.age;
+
+  UserModel.findById(id, (err, user) => {
+    if(err) {
+      res.json(err)
+    };
+    user.name = newName;
+    user.age = newAge;
+
+    user.save();
+  })
+})
+
+app.delete('/deleteUser', (req, res) => {
+  const id = req.body.id;
+
+  UserModel.findByIdAndRemove(id, (err, data) => {
+    if(err) {
+      res.json(err)
+    } else {
+     console.log('deleted')
+    }
+  })
+})
+
+app.listen(3001)
